758d49181c922420dbc650bf72acd799e8f18af1
USUARIO <USUARIO@192.168.51.212>
2014-03-16 10:36:44 -0500

    repositorio vector a matriz

---
+++ src/vectormatriz/VectorMatriz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vectormatriz;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class VectorMatriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        OrdenMatriz tabla = new OrdenMatriz();
+        tabla.setVisible(true);
+    }
+}
